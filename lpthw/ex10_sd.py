30124fa1edf7e88ad9b31094dff9b407b2f4a2cf
Pratyush P Mohapatra <Ativerc@users.noreply.github.com>
2015-07-07 03:06:58 +0530

    ex10_sd.py modified

--- lpthw/ex10_sd.py
+++ lpthw/ex10_sd.py
@@ -16,3 +16,20 @@ print tabby_cat
 print persian_cat
 print backslash_cat
 print fat_cat
+
+#\\     Backslash
+#\'     Single quote
+#\"     Double doute
+#\a     ASCII bell (BEL)
+#\b 	ASCII backspace (BS)
+#\f 	ASCII formfeed (FF)
+#\n 	ASCII linefeed (LF)
+#\N{name} 	Character named name in the Unicode database (Unicode only)
+#\r ASCII 	Carriage Return (CR)
+#\t ASCII 	Horizontal Tab (TAB)
+#\uxxxx 	Character with 16-bit hex value xxxx (Unicode only)
+#\Uxxxxxxxx 	Character with 32-bit hex value xxxxxxxx (Unicode only)
+#\v 	ASCII vertical tab (VT)
+#\ooo 	Character with octal value ooo
+#\xhh 	Character with hex value hh
+
