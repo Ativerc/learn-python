ad626658f8d75e359671689f2b6e5d58530a51db
Pratyush P Mohapatra <Ativerc@users.noreply.github.com>
2015-07-06 22:41:49 +0530

    ex9_sd added

---
+++ lpthw/ex9_sd.py
@@ -0,0 +1,19 @@
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+
+#Changing Zed's "months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"" to
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+#Much more cleaner by adding \n in the beginning.
+
+#printing strings and variable's value in one line. Not new but fun!
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+#Triple quotes > Multi-line strings!
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
