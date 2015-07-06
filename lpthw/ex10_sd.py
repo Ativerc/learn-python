cd4953fd8b04973f0b945d08de879fce7054f11a
Pratyush P Mohapatra <Ativerc@users.noreply.github.com>
2015-07-06 23:24:46 +0530

    ex10_sd.py added. Escape Sequences List

---
+++ lpthw/ex10_sd.py
@@ -0,0 +1,18 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+#Using a single \ for backslash still works for this
+#Dunno why Zed used ""I'm \\ a \\ cat.""
+backslash_cat = "I'm \ a \ cat."
+
+#Using \t for Lists....Hmmm
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
